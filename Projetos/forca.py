d55226b37248ca94efb802336e71aeb6a8994a38
Lucas Soares <lucassoaresf@outlook.com>
2022-10-25 22:45:20 -0300

    Update forca.py

--- Projetos/forca.py
+++ Projetos/forca.py
@@ -4,27 +4,40 @@ def play():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    secret_word = "morango"
+    secret_word = "morango".upper()
     word_true = ["_", "_", "_", "_", "_", "_", "_"]
 
     wrong = False
     right = False
+    error = 0
 
     print(word_true)
 
     while not right and not wrong:
 
         attempt = input("Escolha uma letra: ")
-        attempt = attempt.strip()
+        attempt = attempt.strip().upper()
 
-        index = 0
-        for letter in secret_word:
-            if attempt.upper() == letter.upper():
-                word_true[index] = letter
-            index = index + 1
+        if attempt in secret_word:
+            index = 0
+            for letter in secret_word:
+                if attempt == letter:
+                    word_true[index] = letter
+                index += 1
+        else:
+            error += 1
+            print(f"Você errou! faltam {7-error} tentativas.")
 
+        if error == 7:
+            break
+        if "_" not in word_true:
+            break
         print(word_true)
 
+    if(right):
+        print("Parabéns você acertou a palavra secreta!!!")
+    else:
+        print("Não foi dessa vez que você conseguiu :(")
     print("Fim de jogo!!!")
 
 if __name__ == "__main__":
