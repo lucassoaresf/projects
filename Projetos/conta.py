c55a07670f2018627ae5bfa19a6e23c95f6716f5
Lucas Soares <lucassoaresf@outlook.com>
2022-11-04 15:31:51 -0300

    Update conta.py

--- Projetos/conta.py
+++ Projetos/conta.py
@@ -4,6 +4,17 @@ class Account:
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo um objeto...")
         self.numero = numero
-        self.titulo = titular
+        self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+    def extrato(self):
+        print(f"Saldo {self.saldo} do titular {self.titular}.")
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def sacar(self, valor):
+        self.saldo -= valor
+
+            
