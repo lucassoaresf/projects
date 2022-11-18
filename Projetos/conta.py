0f8da6c45867240cf9715b970554a3bbc748f852
lucassoaresf <lucassoaresf@outlook.com>
2022-11-17 21:29:03 -0300

    update archive

--- Projetos/conta.py
+++ Projetos/conta.py
@@ -3,17 +3,47 @@ class Account:
 
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo um objeto...")
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print(f"Saldo {self.saldo} do titular {self.titular}.")
+        print(f"Saldo {self.__saldo} do titular {self.__titular}.")
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
+
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel
+
 
     def sacar(self, valor):
-        self.saldo -= valor
+        if self.__pode_sacar(valor):
+            self.__saldo -= valor
+        else:
+            print(f"O valor {valor} passou o limite.")
 
+    def transfer(self, valor, destino):
+        self.sacar(valor)
+        destino.deposita(valor)
+
+    def get_saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+    @staticmethod
+    def codigo():
+        return "001"
