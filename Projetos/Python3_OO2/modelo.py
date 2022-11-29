3da79a929f81d31201c20c48a3f1aba1312b3070
lucassoaresf <lucassoaresf@outlook.com>
2022-11-29 20:01:03 -0300

    Update modelo.py

--- Projetos/Python3_OO2/modelo.py
+++ Projetos/Python3_OO2/modelo.py
@@ -1,46 +1,44 @@
-class Filmes:
+class Programa:
+
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_like(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, new_name):
+        self._nome = new_name.title()
+
+class Filmes(Programa):
 
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self.__likes = 0
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def dar_like(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, new_name):
-        self.__nome = new_name.title()
 
 
-class Serie:
+class Serie(Programa):
 
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self.__likes = 0
 
-    @property
-    def likes(self):
-        return self.__likes
 
-    def dar_like(self):
-        self.__likes += 1
+vingadores = Filmes('vingadores - ultimato', 2019, 180)
+vingadores.dar_like()
+print(f'{vingadores.nome} - {vingadores.ano} - {vingadores.duracao} minutos - {vingadores.likes}')
 
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, new_name):
-        self.__nome = new_name.title()
+b99 = Serie('b99', 2013, 8)
+b99.dar_like()
+b99.dar_like()
+print(f'{b99.nome} - {b99.ano} - {b99.temporadas} temporadas - {b99.likes}')
